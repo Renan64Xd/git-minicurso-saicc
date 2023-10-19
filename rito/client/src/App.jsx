aeab22850cdb7e199eaefe0c8d0dd0521cbcb079
Renan_Acosta <107557762+Renan64Xd@users.noreply.github.com>
2023-10-19 11:22:30 -0300

    Add files via upload

---
+++ rito/client/src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import ErrorBoundary from './ErrorBoundary';
+import MainPage from './pages/MainPage';
+
+import ProfileExample from './pages/ProfileExample';
+import BrenoProfile from './pages/profiles/BrenoProfile';
+import ProfileR from './pages/profiles/profileR';
+
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage/>}/>
+
+          <Route element={<DefaultLayout />}>
+            
+            <Route path="/example" element={<ProfileExample/>}/>
+            <Route path="/breno" element={<BrenoProfile/>}/>
+            <Route path="/Rito" element={<ProfileR/>}/>
+
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" />} />
+
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
